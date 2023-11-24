8f29ece7a0b8003e7ce50769bf5cdf1fa0f84973
Nelson2017-8 <nelsonportillo982@gmail.com>
2023-11-24 10:45:05 -0400

    first_commit

---
+++ src/main/java/WorkstationManager.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Nelson
+ */
+public class WorkstationManager {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
